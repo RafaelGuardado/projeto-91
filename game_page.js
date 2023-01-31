b6c55e4abb2db4fe01ccd389aee4a29567746a7f
RafaelGuardado <109107536+RafaelGuardado@users.noreply.github.com>
2023-01-31 15:56:27 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,23 @@
+player1Name = localStorage.getItem(player1Name)
+player2Name = localStorage.getItem(player2Name)
+
+player1Score = 0;
+player2Score = 0;
+
+document.getElementByld("player1Name") innerHtml = player1Name + " : " ;
+document.getElementByld("player2Name") innerHtml = player2Name + " : " ;
+
+document.getElementById(player1Score) innerHtml = player1Score ;
+document.getElementById(player2Score) innerHtml = player2Score ;
+
+document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + player1Name ;
+document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + player2Name ;
+
+function sand () {
+    getWord.Chat = document.getElementById("word").value;
+	word = getWord.toLowerCase();
+	console.log("palavra em caixa baixa = " + word);
+    get
+
+}
+
